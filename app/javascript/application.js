8a5e9d2f8dda7114374f320085ce01ac1021b5a5
Jaydeepsolanki7 <jay.solanki7694@gmail.com>
2024-02-27 16:29:55 +0530

    added bootstrap and jquery in the templates

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,21 @@ import * as bootstrap from "bootstrap"
 import jquery from 'jquery'
 window.jQuery = jquery
 window.$ = jquery
+
+$(document).ready(function() {
+  $("#add-task-button").on("click", function() {
+    console.log("Button clicked!");
+    $("#new-task-form").animate({
+      height: 'toggle'
+    });
+  });
+
+  $(".card").each(function() {
+    var dueDate = new Date($(this).find(".badge").text());
+    if (dueDate < new Date()) {
+      $(this).animate({
+        opacity: 0.15
+      });
+    }
+  });
+});
